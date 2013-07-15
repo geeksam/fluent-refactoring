122eaee8d6517f3cb0b65c708069ac32472e7d46
Sam Livingston-Gray <geeksam@gmail.com>
2013-07-14 21:47:26 -0700

    Wrap entire structure in begin/rescue/end

--- lab/schedule_installation.rb
+++ lab/schedule_installation.rb
@@ -16,42 +16,46 @@ class ScheduleInstallation
       return
     end
 
-    if request.xhr?
-      begin
-        audit_trail_for(current_user) do
-          if @installation.schedule!(desired_date, :installation_type => params[:installation_type], :city => @city)
-            if @installation.scheduled_date
-              date = @installation.scheduled_date.in_time_zone(@installation.city.timezone).to_date
-              render :json => {:errors => nil, :html => schedule_response(@installation, date)}
-            end
-          else
-            render :json => {:errors => [%Q{Could not update installation. #{@installation.errors.full_messages.join(' ')}}] }
-          end
-        end
-      rescue ActiveRecord::RecordInvalid => e
-        render :json => {:errors => [e.message] }
-      rescue ArgumentError => e
-        render :json => {:errors => ["Could not schedule installation. Start by making sure the desired date is on a business day."]}
-      end
-    else
-      begin
-        audit_trail_for(current_user) do
-          if @installation.schedule!(desired_date, :installation_type => params[:installation_type], :city => @city)
-            if @installation.scheduled_date
-              if @installation.customer_provided_equipment?
-                flash[:success] = %Q{Installation scheduled}
-              else
-                flash[:success] = %Q{Installation scheduled! Don't forget to order the equipment also.}
+    begin
+      if request.xhr?
+        begin
+          audit_trail_for(current_user) do
+            if @installation.schedule!(desired_date, :installation_type => params[:installation_type], :city => @city)
+              if @installation.scheduled_date
+                date = @installation.scheduled_date.in_time_zone(@installation.city.timezone).to_date
+                render :json => {:errors => nil, :html => schedule_response(@installation, date)}
               end
+            else
+              render :json => {:errors => [%Q{Could not update installation. #{@installation.errors.full_messages.join(' ')}}] }
             end
-          else
-            flash[:error] = %Q{Could not schedule installation, check the phase of the moon}
           end
+        rescue ActiveRecord::RecordInvalid => e
+          render :json => {:errors => [e.message] }
+        rescue ArgumentError => e
+          render :json => {:errors => ["Could not schedule installation. Start by making sure the desired date is on a business day."]}
         end
-      rescue => e
-        flash[:error] = e.message
+      else
+        begin
+          audit_trail_for(current_user) do
+            if @installation.schedule!(desired_date, :installation_type => params[:installation_type], :city => @city)
+              if @installation.scheduled_date
+                if @installation.customer_provided_equipment?
+                  flash[:success] = %Q{Installation scheduled}
+                else
+                  flash[:success] = %Q{Installation scheduled! Don't forget to order the equipment also.}
+                end
+              end
+            else
+              flash[:error] = %Q{Could not schedule installation, check the phase of the moon}
+            end
+          end
+        rescue => e
+          flash[:error] = e.message
+        end
+        redirect_to(@installation.customer_provided_equipment? ? customer_provided_installations_path : installations_path(:city_id => @installation.city_id, :view => "calendar"))
       end
-      redirect_to(@installation.customer_provided_equipment? ? customer_provided_installations_path : installations_path(:city_id => @installation.city_id, :view => "calendar"))
+    rescue Exception => e
+      raise e
     end
   end
 
